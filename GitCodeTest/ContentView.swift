6ebf220367a8214f3853f83160dd0f4631dc3fc1
Jfram.Hwang <hzw94@naver.com>
2021-02-09 20:33:29 +0900

    Initial Commit

---
+++ GitCodeTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitCodeTest
+//
+//  Created by 황지웅 on 2021/02/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
